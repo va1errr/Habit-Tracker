90571819608048c9753e742fbb93c27aacf91c19
va1errr <valeragladyshev2@gmail.com>
2026-07-21 18:30:26 +0300

    feat: add active habit lookup

--- src/main/java/com/va1err/habittracker/repository/HabitRepository.java
+++ src/main/java/com/va1err/habittracker/repository/HabitRepository.java
@@ -3,6 +3,9 @@ package com.va1err.habittracker.repository;
 import com.va1err.habittracker.entity.Habit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HabitRepository extends JpaRepository<Habit, Long> {
     boolean existsByNameIgnoreCase(String name);
+    List<Habit> findAllByActiveTrue();
 }
